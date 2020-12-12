13dd2dfddf1eba8c04bf3808b610c2b530344af9
Samonnite <981003163@qq.com>
2020-12-12 23:57:06 +0800

    feat: 增加HTML模板 & 多页面配置

--- webpack.config-pages.js
+++ webpack.config-pages.js
@@ -1,3 +1,7 @@
+/**
+ * 多页面配置
+ */
+
 const path = require('path')
 // html模板
 let HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -5,16 +9,14 @@ let HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   // entry: ['./src/index.js', './src/login.js'],    // 入口文件,多入口文件可用数组，但是打包后悔合成一个
-  // entry: { // 多入口文件，以对象形式，打包成多个文件
-  //   index: './src/index.js',
-  //   login: './src/login.js'
-  // },
-  entry: './src/index.js',
+  entry: { // 多入口文件，以对象形式，打包成多个文件
+    index: './src/index.js',
+    login: './src/login.js'
+  },
   output: {   // 出口文件
-    // filename: 'bundle.js', // 打包后的文件名称
     // [name]将和入口文件一一对应
     // 添加hash防止文件缓存
-    filename: 'bundle.[hash:4].js',
+    filename: '[name].[hash:4].js',
     path: path.resolve('dist') // 打包后的文件目录
   },
   module: {},   // 处理对应模块
@@ -24,7 +26,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       // 以index.html作为模板地址
       template: './src/index.html',
-      hash: true, // 是否使用hash
+      filename: 'index.html',
+      chunks: ['index'],
+    }),
+    new HtmlWebpackPlugin({
+      // 以index.html作为模板地址
+      template: './src/login.html',
+      filename: 'login.html',
+      chunks: ['login'],
     })
   ],
   devServer: {},  // 开发服务器配置
